9b94ef6c05a199f50d29c6f30bf9da59837fbb2a
rainwhdmsql-stack <rainwhdmsql@gmail.com>
2026-04-17 11:48:04 +0900

    비즈니스 구조, css, about 구조 및 스크롤 효과 적용. 쌤이랑 똑같이 했는데 안됨;

---
+++ JS/business.js
@@ -0,0 +1,22 @@
+window.addEventListener("load", () => {
+  const swBusiness = new Swiper(".sw-business", {
+    slidesPerView: 1,
+    // slide 간의 간격 (gap)
+    // spaceBetween: 20,
+    breakpoints: {
+      //   480px
+      480: {
+        slidesPerView: 1,
+      },
+      // 640px ~ 1023px
+      640: {
+        slidesPerView: 2,
+      },
+
+      //   1024px ~
+      1024: {
+        slidesPerView: 3,
+      },
+    },
+  });
+});
