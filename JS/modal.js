1c130dfb6ce73c64c0e31238514e5d1c3f160f7a
rainwhdmsql-stack <rainwhdmsql@gmail.com>
2026-04-21 17:10:35 +0900

    모달창 ~

--- JS/modal.js
+++ JS/modal.js
@@ -4,22 +4,6 @@ window.addEventListener("load", function () {
   const modalClose = this.document.querySelector(".modalClose");
   modalClose.addEventListener("click", function () {
     modalWrap.style.display = "none";
-  });
-  // gotop 스크롤 이벤트
-  // 변수 선언
-  const gotop = this.document.querySelector("#gotop");
-  this.window.addEventListener("scroll", () => {
-    if (this.document.documentElement.scrollTop > 1000) {
-      gotop.style.display = "block";
-    } else {
-      gotop.style.display = "none";
-    }
-  });
-  // gotop 버튼 클릭시 위로 스크롤
-  gotop.addEventListener("click", () => {
-    this.window.scrollTo({
-      top: 0,
-      // behavior:"smooth"
-    });
+    console.log(modalClose)
   });
 });
